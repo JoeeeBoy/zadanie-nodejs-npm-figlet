4713af08e04509264fc7ce2fad3d80edeb4d5172
Begaev <ip.huligan@gmail.com>
2022-06-13 13:04:50 +0300

    sdelal

---
+++ app.js
@@ -0,0 +1,8 @@
+import figlet from "figlet";
+console.log(figlet.textSync('intocode!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
